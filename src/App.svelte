305d08df70bff916914bbdff5cfcc4438215db3c
EKI-INDRADI <eki.indradi@gmail.com>
2023-10-08 02:45:42 +0700

    EKI-20231008-019-Deploying Magic Gather Challenge

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+  import Team from "./Team.svelte";
+  let blueScore = 20;
+  let redScore = 20;
+  let winningText = "";
+  $: redWon = blueScore === 0;
+  $: blueWon = redScore === 0;
+  $: gameOver = redWon || blueWon;
+
+  $: if (blueWon) {
+    winningText = "Blue Won!";
+  } else {
+    winningText = "Red Won!";
+  }
+
+  function newGame() {
+    redScore = 20;
+    blueScore = 20;
+  }
+</script>
+
+<!-- div.row>div.col -->
+<div class="row mt-2">
+  <div class="col">
+    <h1 class="text-center">MTG Counter</h1>
+  </div>
+</div>
+
+<div class="row">
+  <Team {gameOver} team="Red" bind:score={redScore} />
+  <Team {gameOver} team="Blue" bind:score={blueScore} />
+</div>
+
+{#if !gameOver}
+  <!-- div.row -->
+  <div class="row mt-3">
+    <!-- div.col -->
+    <div class="col">
+      <!-- button.btn.btn-warning -->
+      <button on:click={newGame} class="btn btn-warning w-100"
+        >Reset Game
+      </button>
+    </div>
+  </div>
+{:else}
+  <div class="row mt-3">
+    <!-- div.col -->
+    <div class="col">
+      <!-- <h2>{winningText}</h2> -->
+      {#if blueWon}
+        <!-- h2.text-center -->
+        <h2 class="text-center text-primary">Blue Won</h2>
+      {:else}
+        <h2 class="text-center text-danger">Red Won</h2>
+      {/if}
+    </div>
+  </div>
+  <div class="row mt-3">
+    <div class="col">
+      <button on:click={newGame} class="btn btn-success w-100"
+        >New Game
+      </button>
+    </div>
+  </div>
+{/if}
---
+++ src/Team.svelte
@@ -0,0 +1,31 @@
+<script>
+    // export let team = "Red";
+    // export let score = 20;
+    export let team;
+    export let score;
+    export let gameOver;
+
+    function add() {
+        score += 1;
+    }
+
+    function minus() {
+        score -= 1;
+    }
+</script>
+
+<div style="border : dashed {team.toLowerCase()} 5px;" class="col">
+    <h2 class:text-danger={team === "Red"} class:text-primary={team === "Blue"}>
+        Team {team} : {score}
+    </h2>
+    <!-- button.btn.btn-success -->
+    <button disabled={gameOver} on:click={add} class="btn btn-success">+</button>
+    <button disabled={gameOver} on:click={minus} class="btn btn-danger">-</button>
+</div>
+
+<style>
+    div.col {
+        padding: 20px;
+        text-align: center;
+    }
+</style>
